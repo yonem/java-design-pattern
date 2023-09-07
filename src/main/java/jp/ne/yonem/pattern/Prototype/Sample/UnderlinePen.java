15479d042a9f535e122bfb8e69ab2bc7d95413fa
a-yonekawa <yonem321@gmail.com>
2023-09-08 00:52:56 +0900

    feat: Prototypeパターンを実装する #18

---
+++ src/main/java/jp/ne/yonem/pattern/Prototype/Sample/UnderlinePen.java
@@ -0,0 +1,34 @@
+package jp.ne.yonem.pattern.Prototype.Sample;
+
+
+import jp.ne.yonem.pattern.Prototype.Sample.framework.Product;
+
+public class UnderlinePen implements Product {
+    private final char ulchar;
+
+    public UnderlinePen(char ulchar) {
+        this.ulchar = ulchar;
+    }
+
+    public void use(String s) {
+        var length = s.getBytes().length;
+        System.out.println("\"" + s + "\"");
+        System.out.print(" ");
+
+        for (var i = 0; i < length; i++) {
+            System.out.print(ulchar);
+        }
+        System.out.println();
+    }
+
+    public Product createClone() {
+
+        try {
+            return (Product) clone();
+
+        } catch (CloneNotSupportedException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}

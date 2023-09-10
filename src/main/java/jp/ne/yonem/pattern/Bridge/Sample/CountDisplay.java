ae72504bd5c1e069107c0b335a4c43e77a2a213e
a-yonekawa <yonem321@gmail.com>
2023-09-10 11:21:09 +0900

    feat: Bridgeパターンを実装する #23

---
+++ src/main/java/jp/ne/yonem/pattern/Bridge/Sample/CountDisplay.java
@@ -0,0 +1,17 @@
+package jp.ne.yonem.pattern.Bridge.Sample;
+
+public class CountDisplay extends Display {
+
+    public CountDisplay(DisplayImpl impl) {
+        super(impl);
+    }
+
+    public void multiDisplay(int times) {       // times回繰り返して表示する
+        open();
+
+        for (var i = 0; i < times; i++) {
+            print();
+        }
+        close();
+    }
+}

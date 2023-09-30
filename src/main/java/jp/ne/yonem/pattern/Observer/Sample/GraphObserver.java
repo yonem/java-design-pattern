a913d6540354c50cdb95a896e4fcbd0278ed5126
a-yonekawa <yonem321@gmail.com>
2023-09-30 13:09:20 +0900

    feat: Observerパターンを実装する #40

---
+++ src/main/java/jp/ne/yonem/pattern/Observer/Sample/GraphObserver.java
@@ -0,0 +1,20 @@
+package jp.ne.yonem.pattern.Observer.Sample;
+
+public class GraphObserver implements Observer {
+
+    public void update(NumberGenerator generator) {
+        System.out.print("GraphObserver:");
+        var count = generator.getNumber();
+
+        for (var i = 0; i < count; i++) {
+            System.out.print("*");
+        }
+        System.out.println();
+
+        try {
+            Thread.sleep(100);
+            
+        } catch (InterruptedException ignored) {
+        }
+    }
+}
---
+++ src/main/java/jp/ne/yonem/pattern/Observer/Sample/Observer.java
@@ -0,0 +1,5 @@
+package jp.ne.yonem.pattern.Observer.Sample;
+
+public interface Observer {
+    void update(NumberGenerator generator);
+}

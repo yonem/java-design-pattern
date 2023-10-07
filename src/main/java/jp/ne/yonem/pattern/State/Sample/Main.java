b1c2577cceb94b6258e329d7ebf967312596e290
a-yonekawa <yonem321@gmail.com>
2023-10-07 18:25:47 +0900

    feat: Stateパターンを実装する #46

---
+++ src/main/java/jp/ne/yonem/pattern/State/Sample/Main.java
@@ -0,0 +1,20 @@
+package jp.ne.yonem.pattern.State.Sample;
+
+public class Main {
+
+    public static void main(String[] args) {
+        var frame = new SafeFrame("State Sample");
+
+        while (true) {
+            for (var hour = 0; hour < 24; hour++) {
+                frame.setClock(hour);   // 時刻の設定
+
+                try {
+                    Thread.sleep(1000);
+
+                } catch (InterruptedException ignored) {
+                }
+            }
+        }
+    }
+}

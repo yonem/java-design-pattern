c602a3768a053e6fb8676af8f4342a69cd658ea7
a-yonekawa <yonem321@gmail.com>
2023-10-13 19:35:48 +0900

    refactor: リファクタリング実施 #58

--- src/main/java/jp/ne/yonem/pattern/FactoryMethod/A2/Main.java
+++ src/main/java/jp/ne/yonem/pattern/FactoryMethod/A2/Main.java
@@ -8,7 +8,7 @@ public class Main {
 
     public static void main(String[] args) {
         Factory factory = new IDCardFactory();
-        Product card1 = factory.create("結城浩");
+        Product card1 = factory.create("蛇場太郎");
         Product card2 = factory.create("とむら");
         Product card3 = factory.create("佐藤花子");
         card1.use();

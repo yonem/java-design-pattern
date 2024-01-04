91461c4ad40735020ed33819d99b9a922752f597
a-yonekawa <yonem321@gmail.com>
2024-01-05 00:36:13 +0900

    feat: TemplateMethodを是正する #146

--- src/main/java/jp/ne/yonem/pattern/TemplateMethod/Sample/Main.java
+++ src/main/java/jp/ne/yonem/pattern/TemplateMethod/Sample/Main.java
@@ -3,9 +3,9 @@ package jp.ne.yonem.pattern.TemplateMethod.Sample;
 public class Main {
 
     public static void main(String[] args) {
-        AbstractDisplay d1 = new CharDisplay('H');                  // 'H'を持ったCharDisplayのインスタンスを1個作る。
-        AbstractDisplay d2 = new StringDisplay("Hello, world.");    // "Hello, world."を持ったStringDisplayのインスタンスを1個作る。
-        AbstractDisplay d3 = new StringDisplay("こんにちは。");       // "こんにちは。"を持ったStringDisplayのインスタンスを1個作る。
+        var d1 = new CharDisplay('H');                  // 'H'を持ったCharDisplayのインスタンスを1個作る。
+        var d2 = new StringDisplay("Hello, world.");    // "Hello, world."を持ったStringDisplayのインスタンスを1個作る。
+        var d3 = new StringDisplay("こんにちは。");       // "こんにちは。"を持ったStringDisplayのインスタンスを1個作る。
         d1.display();                                               // d1,d2,d3とも、すべて同じAbstractDisplayのサブクラスのインスタンスだから、
         d2.display();                                               // 継承したdisplayメソッドを呼び出すことができる。
         d3.display();                                               // 実際の動作は個々のクラスCharDisplayやStringDisplayで定まる。
